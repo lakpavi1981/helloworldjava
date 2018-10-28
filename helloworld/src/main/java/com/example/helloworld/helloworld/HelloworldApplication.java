955820aeda0f232345b8f622e173d6766ef41603
Murali Krishnamoorthy <murali.krishnamoorthy@thermofisher.com>
2018-10-27 21:50:47 -0400

    unit test added

--- helloworld/src/main/java/com/example/helloworld/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/com/example/helloworld/helloworld/HelloworldApplication.java
@@ -9,6 +9,12 @@ public class HelloworldApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(HelloworldApplication.class, args);
-		System.out.println("Hellow World")
+
+	}
+
+	public static String helloworldprint(){
+		System.out.println("Hellow World");
+		return "HelloWorld";
+
 	}
 }
--- helloworld/src/test/java/com/example/helloworld/helloworld/HelloworldApplicationTests.java
+++ helloworld/src/test/java/com/example/helloworld/helloworld/HelloworldApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.helloworld.helloworld;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class HelloworldApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Assert.assertEquals("HelloWorld" , HelloworldApplication.helloworldprint());
 	}
 
 }
